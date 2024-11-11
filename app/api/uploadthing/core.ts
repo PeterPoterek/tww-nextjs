101e3575ffb4b031ef027776b51493fb1b397469
Piotr Poterek <peterpoterek0@gmail.com>
2024-11-11 18:28:47 +0100

    upload image to the db

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -21,13 +21,26 @@ export const ourFileRouter = {
       }
 
       // Return user ID to pass to the onUploadComplete callback
-      return { userId: session.user.name };
+      return { userId: session.user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
 
       console.log("file url", file.url);
+
+      try {
+        await prisma.galleryImage.create({
+          data: {
+            url: file.url as string,
+            userId: metadata.userId as string,
+          },
+        });
+
+        console.log("Image record successfully created in the database");
+      } catch (error) {
+        console.error("Error saving image record to the database:", error);
+      }
 
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId };
