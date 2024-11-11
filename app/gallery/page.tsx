82aa0dec4df1a19d3dc0783fd4921a6a620cf4a2
Piotr Poterek <peterpoterek0@gmail.com>
2024-11-11 18:58:23 +0100

    render images from db

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,5 +1,22 @@
-const page = () => {
-  return <h1>Gallery</h1>;
+import prisma from "@/lib/db";
+import Image from "next/image";
+
+const page = async () => {
+  const gallery = await prisma.galleryImage.findMany();
+
+  console.log(gallery);
+
+  return (
+    <ul className="flex gap-5 p-5 justify-center items-center">
+      {gallery.map((image, index) => {
+        return (
+          <li key={index} className="w-80">
+            <Image src={image.url} key={image.id} alt="image" width={500} height={350} />
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default page;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME || "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
