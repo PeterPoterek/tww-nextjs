2080e86ae42a18bc140dfd7c09d5814b747bc2f1
Piotr Poterek <peterpoterek0@gmail.com>
2024-11-21 02:20:09 +0100

    banner

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import AboutSection from "@/components/about/about-section";
 import HeroSection from "@/components/hero/hero-section";
 import StatsSection from "@/components/stats/stats-section";
 import CreationSection from "@/components/creation/creation-section";
+import BannerContainer from "@/components/banner/banner-container";
 
 export default async function Home() {
   return (
-    <main>
+    <main className={"bg-stone-900"}>
       <HeroSection />
 
       <AboutSection />
@@ -13,6 +14,8 @@ export default async function Home() {
       <StatsSection />
 
       <CreationSection />
+
+      <BannerContainer />
     </main>
   );
 }
---
+++ components/banner/banner-container.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+const BannerContainer = () => {
+  return (
+    <div className="relative w-full">
+      <Image
+        src="/banner.png"
+        alt="Banner background"
+        width={1920}
+        height={300}
+        className="w-full h-[150px] sm:h-[200px] md:h-[250px] lg:h-[300px] object-cover"
+        priority
+      />
+      <div className="absolute inset-0 flex items-center justify-center bg-opacity-50">
+        <h3
+          className="uppercase text-center font-black text-white px-4 py-2 max-w-4xl
+                       text-lg sm:text-xl md:text-2xl lg:text-3xl
+                       leading-tight sm:leading-tight md:leading-tight lg:leading-tight"
+        >
+          ZADZWOŃ I ODBIERZ BEZPŁATNY KOSZTORYS
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default BannerContainer;
