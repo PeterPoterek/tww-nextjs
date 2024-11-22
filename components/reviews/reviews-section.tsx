6088aa87fb1f4d1f6de5da7c168d92e0d0e6fae1
Piotr Poterek <peterpoterek0@gmail.com>
2024-11-22 03:59:05 +0100

    Update reviews-section.tsx

--- components/reviews/reviews-section.tsx
+++ components/reviews/reviews-section.tsx
@@ -9,18 +9,26 @@ const ReviewsSection = () => {
 
       <div
         className={
-          "flex flex-col items-center py-[3.5rem] mt-20 bg-slate-50 text-secondary border-l-[16px] border-sky-800"
+          "relative flex flex-col items-center py-[3.5rem] mt-20 bg-slate-50 text-secondary border-l-[16px] border-sky-800"
         }
       >
-        <p className={"max-w-[473px] text-xl text-center font-light"}>
+        <p
+          className={
+            "relative max-w-[473px] text-xl text-center font-light before:content-['“'] after:content-['”'] before:text-sky-800 after:text-sky-800 before:text-5xl after:text-5xl before:leading-none after:leading-none before:absolute before:-top-4 before:-left-6 after:absolute after:-bottom-4 after:-right-6"
+          }
+        >
           Super fachowcy, punktualni, odpowiedzialni, rzeczowi i bardzo
           profesjonalni. Wykonawcy bardzo dokładni, rzetelni i solidni. Panowie
           Piotr i Paweł to zgrany zespół. Terminowo i kompleksowo wykończyli
           mieszkanie. Są bezkonfliktowi, służą doświadczeniem i doradztwem. Dla
           mnie super! Polecam na 120%.
         </p>
-        <div className={"flex flex-end"}>
-          <span className={"text-xl font-bold"}>Michał Makowski</span>
+        <div
+          className={
+            "absolute bottom-4 right-20 text-xl font-bold text-secondary"
+          }
+        >
+          Michał Makowski
         </div>
       </div>
     </section>
