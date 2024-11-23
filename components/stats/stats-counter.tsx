002efb4ea627d98cf914cac9b17644611f82cb54
Piotr Poterek <peterpoterek0@gmail.com>
2024-11-23 23:11:19 +0100

    stats counter

---
+++ components/stats/stats-counter.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import { useInView } from "framer-motion";
+
+const StatsCounter = ({ value }: { value: number }) => {
+  const [currentValue, setCurrentValue] = useState(0);
+  const ref = useRef<HTMLSpanElement>(null);
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      const duration = 1000;
+      const startTime = performance.now();
+
+      const animate = (currentTime: number) => {
+        const elapsedTime = currentTime - startTime;
+        const progress = Math.min(elapsedTime / duration, 1);
+        const newValue = Math.max(Math.floor(progress * value), 0);
+        setCurrentValue(newValue);
+
+        if (progress < 1) {
+          requestAnimationFrame(animate);
+        }
+      };
+
+      requestAnimationFrame(animate);
+    }
+  }, [isInView, value]);
+
+  return (
+    <span ref={ref} className="text-2xl md:text-3xl">
+      {currentValue}
+    </span>
+  );
+};
+export default StatsCounter;
