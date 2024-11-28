bd834563785ecfd0200b102e83b167b373445881
Piotr Poterek <peterpoterek0@gmail.com>
2024-11-28 20:57:56 +0100

    Update navbar-link.tsx

--- components/navbar/navbar-link.tsx
+++ components/navbar/navbar-link.tsx
@@ -1,37 +1,25 @@
 "use client";
 
+import Link from "next/link";
 import { motion } from "framer-motion";
-import { useRouter } from "next/navigation";
 
 interface NavbarLinkProps {
   path: string;
   text: string;
-  scroll?: boolean;
   onClick?: () => void;
 }
 
 const NavbarLink = ({ path, text, onClick }: NavbarLinkProps) => {
-  const router = useRouter();
-
-  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault();
-    if (onClick) {
-      onClick();
-    }
-    router.push(path);
-    window.scrollTo(0, 0);
-  };
-
   return (
     <li className="cursor-pointer text-primary">
-      <a href={path} onClick={handleClick}>
+      <Link href={path} onClick={onClick}>
         <motion.div
           whileHover={{ color: "#0288d1" }}
           transition={{ duration: 0.3 }}
         >
           {text}
         </motion.div>
-      </a>
+      </Link>
     </li>
   );
 };
