8ec92da8a466e5c559e3f791dd97a9f28b3c17c2
Piotr Poterek <peterpoterek0@gmail.com>
2024-11-29 01:40:29 +0100

    Update Map.tsx

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -1,10 +1,11 @@
 "use client";
+
 import React, { useState } from "react";
 import {
   GoogleMap,
-  LoadScriptNext,
   Marker,
   InfoWindow,
+  useJsApiLoader,
 } from "@react-google-maps/api";
 
 const containerStyle = {
@@ -27,21 +28,31 @@ if (!apiKey) {
 const Map = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: apiKey,
+  });
+
   return (
-    <LoadScriptNext googleMapsApiKey={apiKey}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
-        <Marker position={center} onClick={() => setIsOpen(true)} />
-        {isOpen && (
-          <InfoWindow position={center} onCloseClick={() => setIsOpen(false)}>
-            <div>
-              <h4 className={"text-secondary font-black"}>
-                Technologia Wykonana Wnęrz
-              </h4>
-            </div>
-          </InfoWindow>
-        )}
-      </GoogleMap>
-    </LoadScriptNext>
+    <div style={containerStyle}>
+      {!isLoaded ? (
+        <div className="flex items-center justify-center h-full">
+          <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 border-t-transparent rounded-full text-primary"></div>
+        </div>
+      ) : (
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
+          <Marker position={center} onClick={() => setIsOpen(true)} />
+          {isOpen && (
+            <InfoWindow position={center} onCloseClick={() => setIsOpen(false)}>
+              <div>
+                <h4 className={"text-secondary font-black"}>
+                  Technologia Wykonana Wnętrz
+                </h4>
+              </div>
+            </InfoWindow>
+          )}
+        </GoogleMap>
+      )}
+    </div>
   );
 };
 
