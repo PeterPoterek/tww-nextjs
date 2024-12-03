7a8a15205887d226a9540f87af4fa759f049a743
Piotr Poterek <peterpoterek0@gmail.com>
2024-12-03 03:08:49 +0100

    Update stats-counter.tsx

--- components/stats/stats-counter.tsx
+++ components/stats/stats-counter.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, useCallback } from "react";
 import { useInView } from "framer-motion";
 
 interface StatsCounterProps {
@@ -7,36 +7,45 @@ interface StatsCounterProps {
   projects?: boolean;
 }
 
-const StatsCounter = ({ value, projects = false }: StatsCounterProps) => {
-  const [currentValue, setCurrentValue] = useState(0);
-  const ref = useRef<HTMLSpanElement>(null);
-  const isInView = useInView(ref, { once: true });
+const StatsCounter = React.memo(
+  ({ value, projects = false }: StatsCounterProps) => {
+    const [currentValue, setCurrentValue] = useState(0);
+    const ref = useRef<HTMLSpanElement>(null);
+    const isInView = useInView(ref, { once: true });
 
-  useEffect(() => {
-    if (isInView) {
-      const duration = 1000;
-      const startTime = performance.now();
-
-      const animate = (currentTime: number) => {
+    const animate = useCallback(
+      (startTime: number) => {
+        const duration = 1000;
+        const currentTime = performance.now();
         const elapsedTime = currentTime - startTime;
         const progress = Math.min(elapsedTime / duration, 1);
-        const newValue = Math.max(Math.floor(progress * value), 0);
+        const newValue = Math.floor(progress * value);
+
         setCurrentValue(newValue);
 
         if (progress < 1) {
-          requestAnimationFrame(animate);
+          requestAnimationFrame(() => animate(startTime));
         }
-      };
+      },
+      [value],
+    );
 
-      requestAnimationFrame(animate);
-    }
-  }, [isInView, value]);
+    useEffect(() => {
+      if (isInView) {
+        const startTime = performance.now();
+        animate(startTime);
+      }
+    }, [isInView, animate]);
 
-  return (
-    <span ref={ref} className="text-2xl md:text-3xl">
-      {currentValue}
-      {projects && "+"}
-    </span>
-  );
-};
+    return (
+      <span ref={ref} className="text-2xl md:text-3xl">
+        {currentValue}
+        {projects && "+"}
+      </span>
+    );
+  },
+);
+
+StatsCounter.displayName = "StatsCounter";
+
 export default StatsCounter;
